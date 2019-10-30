2747ef855447ff67b07cd197c7c0227898f475e0
Shixin Yang <shixiny@sfu.ca>
2019-10-30 13:40:12 -0700

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar navbar-light" style={myStyle}>
+            <p>Event Finder</p>
+        </div>
+    )
+}
+
+export default Navbar
+
+const myStyle = {backgroundColor: '#e3f2fd'}
