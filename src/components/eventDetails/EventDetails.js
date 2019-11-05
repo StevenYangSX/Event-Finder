27c4853203f1fa667a7b4510b39bd73b3ee0e43b
Shixin Yang <shixiny@sfu.ca>
2019-11-05 13:01:37 -0800

    Refactored all component to functional

--- src/components/eventDetails/EventDetails.js
+++ src/components/eventDetails/EventDetails.js
@@ -1,58 +1,64 @@
-import React, { Component, Fragment } from 'react'
-import {Link} from 'react-router-dom';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-class EventDetails extends Component {
-    state = {
-        event: {},
-        loading : true
-    }
+import React, { useState, useEffect, Fragment } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-    eventDetails = async (eventId) => {
-        console.log("eventId is:!!!:", eventId)
-        const res = await axios.get(`https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?&apikey=d5jyQtKEHAXiqyDMCSVsdid5ooEqm5Pg`)
-        //const res =  await response.json();
-       
-        this.setState({event: res.data,loading: false});
-        console.log("event object is:!!!:", res.data)
-      }
-    
-    componentDidMount() {
-        this.eventDetails(this.props.match.params.eventId);
-        
-    }
-    
-   
-    
-    render() {
-        
-        if(this.state.loading === false) {
-            return (
-                <Fragment>
-                    <div className="container">
-                        <Link to='/' className='btn btn-light'>Back to Search</Link>
-                        <div className="card grid-2">
-                            <div className="all-center">
-                                <img src={this.state.event.images[0].url} className='round-img' style={{width: '150px'}} alt=""/>
-                                <h1>{this.state.event.name}</h1>
-                                <p>Time: {this.state.event.dates.start.localTime} {this.state.event.dates.start.localDate}</p>
-                                <p>Location: {this.state.event._embedded.venues[0].address.line1}, {this.state.event._embedded.venues[0].name}, {this.state.event._embedded.venues[0].city.name}, {this.state.event._embedded.venues[0].country.name}</p>
-                                <a href={this.state.event.url} rel="noopener noreferrer" target="_blank">Go for Tickets</a>             
-                            </div>
-                        </div>
-                    </div>
-                </Fragment>     
-            )
-    
-        }else {
-            return (
-                <div>
-                    <p>Loading...</p>
-                </div>
-            )
-        }
-        
-    }
+const EventDetails = props => {
+  const [event, setEvent] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    eventDetails(props.match.params.eventId)
+  }, [])
+  const eventDetails = async eventId => {
+    const res = await axios.get(
+      `https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?&apikey=d5jyQtKEHAXiqyDMCSVsdid5ooEqm5Pg`
+    )
+    setEvent(res.data)
+    setLoading(false)
+  }
+
+  if (loading === false) {
+    return (
+      <Fragment>
+        <div className="container">
+          <Link to="/" className="btn btn-light">
+            Back to Search
+          </Link>
+          <div className="card grid-2">
+            <div className="all-center">
+              <img
+                src={event.images[0].url}
+                className="round-img"
+                style={{ width: "150px" }}
+                alt=""
+              />
+              <h1>{event.name}</h1>
+              <p>
+                Time: {event.dates.start.localTime}{" "}
+                {event.dates.start.localDate}
+              </p>
+              <p>
+                Location: {event._embedded.venues[0].address.line1},{" "}
+                {event._embedded.venues[0].name},{" "}
+                {event._embedded.venues[0].city.name},{" "}
+                {event._embedded.venues[0].country.name}
+              </p>
+              <a href={event.url} rel="noopener noreferrer" target="_blank">
+                Go for Tickets
+              </a>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    )
+  } else {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    )
+  }
 }
 
 export default EventDetails
