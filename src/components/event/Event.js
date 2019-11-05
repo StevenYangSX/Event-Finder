75d311f9798d53211bdabfd0404b71d552d40757
Shixin Yang <shixiny@sfu.ca>
2019-11-05 13:01:37 -0800

    Refactored all component to functional

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from 'react-router-dom';
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Link } from "react-router-dom"
 
-class Event extends Component {
-    
-    render() {
-        return (
-            // <div>
-            //     <h4>{this.props.title}</h4>
-            //     <p>{this.props.intro}</p>
-            //     <p>{this.props.time}</p>
-            //     
-            // </div>
-            <div className="card flex-column">       
-                <img className="card-img-top" src={this.props.image} alt=""/>    
-                <div className="card card-body h-100 flex-column">
-                    <h4 className="card-title h-100">{this.props.title}</h4>
-                    <p className="card-text ">{this.props.time}</p>
-                    <Link to={`/event/${this.props.eventId}`} className="btn btn-dark">Details</Link>   
-                </div>
-            </div>
-
-        )
-    }
+const Event = ({ image, title, time, eventId }) => {
+  return (
+    // </div>
+    <div className="card flex-column">
+      <img className="card-img-top" src={image} alt="" />
+      <div className="card card-body h-100 flex-column">
+        <h4 className="card-title h-100">{title}</h4>
+        <p className="card-text ">{time}</p>
+        <Link to={`/event/${eventId}`} className="btn btn-dark">
+          Details
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Event
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,41 +1,44 @@
-import React,{Component} from 'react';
-import {Navbar , Nav, Form,FormControl, Button} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react"
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-class NavBar extends Component {
-    state = {
-        city : '',
-      }
+const NavBar = props => {
+  const [city, setCity] = useState("")
 
-    onChange = (e) => {
-        this.setState({city: e.target.value}) 
-    }
-    onClick = e => {
-        e.preventDefault();
-        this.props.onClick(this.state.city);
-        this.setState({city: ''});
-    }
-    render(){
-        return (
-            <Navbar bg="dark" expand="md" variant="dark">
-                <Navbar.Brand href="/"><i className="fas fa-icons"></i> Event Finder</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="artists" className="mr-sm-2" onChange={this.onChange} />
-                        <Button  variant="secondary" onClick={this.onClick}>Search</Button>
-                   
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
-          )
-    }
-    
-    }
+  const onChange = e => {
+    setCity(e.target.value)
+  }
+  const onClick = e => {
+    e.preventDefault()
+    props.onClick(city)
+    setCity("")
+  }
+
+  return (
+    <Navbar bg="dark" expand="md" variant="dark">
+      <Navbar.Brand href="/">
+        <i className="fas fa-icons"></i> Event Finder
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="artists"
+            className="mr-sm-2"
+            onChange={onChange}
+          />
+          <Button variant="secondary" onClick={onClick}>
+            Search
+          </Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
 
 export default NavBar
-
